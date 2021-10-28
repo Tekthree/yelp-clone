52dbf0de568f81e2e65a19d63509c0c9c52d3fdf
Tek Jones <tekthree@gmail.com>
2021-10-27 21:55:59 -0700

    working on explorer page, javascript

---
+++ camps.js
@@ -0,0 +1,175 @@
+import buloy from "./assets/campImages/Buloy-Springs.jpg";
+import calaguas from "./assets/campImages/Calaguas-Island.jpg";
+import latik from "./assets/campImages/Latik-Riverside.jpg";
+import ulap from "./assets/campImages/Mount-Ulap.jpg";
+import onay from "./assets/campImages/Onay-Beach.jpg";
+import seven_sisters from "./assets/campImages/Seven-Sisters-Waterfall.jpg";
+
+
+const camps = [
+  {
+    id: new Date().getTime().toString(),
+    img: ulap,
+    slug: "mount-ulap",
+    title: "Mount Ulap",
+    info: "One of the most famous hikes in Benguet is Mt Ulap in Itogon.",
+    full_info:
+      "Mount Ulap is a 7.7 kilometer moderately trafficked point-to-point trail located near Tuba, Benguet, Philippines that offers the chance to see wildlife and is rated as moderate. The trail is primarily used for hiking.",
+    price: "$104.99",
+    reviews: [
+      {
+        name: "Adam Jones",
+        time: "13h ago",
+        info: "Honestly one of the best experiences ever, took us a while to figure out how to get there but it was amazing!",
+      },
+      {
+        name: "Isaac Dylan",
+        time: "1 day ago",
+        info: "Travelling changes you as a person, you gain more perspective, this is the perfect spot to do that.",
+      },
+      {
+        name: "Hudson Luca",
+        time: "3 days ago",
+        info: "Definitely recommend going there, not too far and not a lot of people to ruin the experience.",
+      },
+    ],
+  },
+  {
+    id: new Date().getTime().toString(),
+    img: calaguas,
+    slug: "calaguas-islands",
+    title: "Calaguas Islands",
+    info: "A paradise of islands that can rival the white sand beauty of Boracay.",
+    full_info:
+      "Mount Ulap is a 7.7 kilometer moderately trafficked point-to-point trail located near Tuba, Benguet, Philippines that offers the chance to see wildlife and is rated as moderate. The trail is primarily used for hiking.",
+    price: "$104.99",
+    reviews: [
+      {
+        name: "Adam Jones",
+        time: "13h ago",
+        info: "Honestly one of the best experiences ever, took us a while to figure out how to get there but it was amazing!",
+      },
+      {
+        name: "Isaac Dylan",
+        time: "1 day ago",
+        info: "Travelling changes you as a person, you gain more perspective, this is the perfect spot to do that.",
+      },
+      {
+        name: "Hudson Luca",
+        time: "3 days ago",
+        info: "Definitely recommend going there, not too far and not a lot of people to ruin the experience.",
+      },
+    ],
+  },
+  {
+    id: new Date().getTime().toString(),
+    img: onay,
+    slug: "onay-beach",
+    title: "Onay Beach",
+    info: "This is one of the best beach camping sites, beautiful and pristine.",
+    full_info:
+      "Mount Ulap is a 7.7 kilometer moderately trafficked point-to-point trail located near Tuba, Benguet, Philippines that offers the chance to see wildlife and is rated as moderate. The trail is primarily used for hiking.",
+    price: "$104.99",
+    reviews: [
+      {
+        name: "Adam Jones",
+        time: "13h ago",
+        info: "Honestly one of the best experiences ever, took us a while to figure out how to get there but it was amazing!",
+      },
+      {
+        name: "Isaac Dylan",
+        time: "1 day ago",
+        info: "Travelling changes you as a person, you gain more perspective, this is the perfect spot to do that.",
+      },
+      {
+        name: "Hudson Luca",
+        time: "3 days ago",
+        info: "Definitely recommend going there, not too far and not a lot of people to ruin the experience.",
+      },
+    ],
+  },
+  {
+    id: new Date().getTime().toString(),
+    img: seven_sisters,
+    slug: "seven-sisters-waterfall",
+    title: "Seven Sisters Waterfall",
+    info: "The Seven Sisters is the 39th tallest waterfall in Norway.",
+    full_info:
+      "Mount Ulap is a 7.7 kilometer moderately trafficked point-to-point trail located near Tuba, Benguet, Philippines that offers the chance to see wildlife and is rated as moderate. The trail is primarily used for hiking.",
+    price: "$104.99",
+    reviews: [
+      {
+        name: "Adam Jones",
+        time: "13h ago",
+        info: "Honestly one of the best experiences ever, took us a while to figure out how to get there but it was amazing!",
+      },
+      {
+        name: "Isaac Dylan",
+        time: "1 day ago",
+        info: "Travelling changes you as a person, you gain more perspective, this is the perfect spot to do that.",
+      },
+      {
+        name: "Hudson Luca",
+        time: "3 days ago",
+        info: "Definitely recommend going there, not too far and not a lot of people to ruin the experience.",
+      },
+    ],
+  },
+  {
+    id: new Date().getTime().toString(),
+    img: latik,
+    slug: "latik-riverside",
+    title: "Latik Riverside",
+    info: "Philippines is one of the most dazzling countries in all of Asia.",
+    full_info:
+      "Mount Ulap is a 7.7 kilometer moderately trafficked point-to-point trail located near Tuba, Benguet, Philippines that offers the chance to see wildlife and is rated as moderate. The trail is primarily used for hiking.",
+    price: "$104.99",
+    reviews: [
+      {
+        name: "Adam Jones",
+        time: "13h ago",
+        info: "Honestly one of the best experiences ever, took us a while to figure out how to get there but it was amazing!",
+      },
+      {
+        name: "Isaac Dylan",
+        time: "1 day ago",
+        info: "Travelling changes you as a person, you gain more perspective, this is the perfect spot to do that.",
+      },
+      {
+        name: "Hudson Luca",
+        time: "3 days ago",
+        info: "Definitely recommend going there, not too far and not a lot of people to ruin the experience.",
+      },
+    ],
+  },
+  {
+    id: new Date().getTime().toString(),
+    img: buloy,
+    slug: "buloy-springs",
+    title: "Buloy Springs",
+    info: "This is one of the best beach camping sites, beautiful and pristine.",
+    full_info:
+      "Mount Ulap is a 7.7 kilometer moderately trafficked point-to-point trail located near Tuba, Benguet, Philippines that offers the chance to see wildlife and is rated as moderate. The trail is primarily used for hiking.",
+    price: "$104.99",
+    reviews: [
+      {
+        name: "Adam Jones",
+        time: "13h ago",
+        info: "Honestly one of the best experiences ever, took us a while to figure out how to get there but it was amazing!",
+      },
+      {
+        name: "Isaac Dylan",
+        time: "1 day ago",
+        info: "Travelling changes you as a person, you gain more perspective, this is the perfect spot to do that.",
+      },
+      {
+        name: "Hudson Luca",
+        time: "3 days ago",
+        info: "Definitely recommend going there, not too far and not a lot of people to ruin the experience.",
+      },
+    ],
+  },
+];
+
+
+export default camps 
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 import './style.css'
+import camps from './camps.js'
 
 
+
+$('.grid-image').attr("src",camps[0].img)
+console.log(camps[0].img)
